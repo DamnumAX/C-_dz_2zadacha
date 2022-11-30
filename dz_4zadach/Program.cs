ee3993d6d4863d10cd08d4da9fd5e2b10cb27f87
Filipp G <filfilet@mail.ru>
2022-11-30 17:04:46 +0300

    first commit

---
+++ dz_4zadach/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num_a = 2;
+
+Console.WriteLine("Введите второе число: ");
+int num_b = 3;
+
+Console.WriteLine("Введите третье число: ");
+int num_c = 7;
+
+int max = num_a;
+
+if (num_b > max)
+{
+max = num_b;
+}
+
+if (num_c > max)
+{
+max = num_c;
+}
+
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
